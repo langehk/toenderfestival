54199b588f8f099bf436ac88156891c59386bec0
langehk <32093132+langehk@users.noreply.github.com>
2020-11-26 13:08:29 +0100

    add multiple files

--- validateForm.js
+++ validateForm.js
@@ -32,7 +32,7 @@ function validateForm(){
     
 }
 
-
+// Dropdown function. Shows the selected item.
 $(".dropdown-menu li a").click(function(){
     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
@@ -40,7 +40,35 @@ $(".dropdown-menu li a").click(function(){
 
 
 
+  $(document).ready(function(){
 
-
-
-
+    $('#submitPhoto').click(function(){
+    
+       var form_data = new FormData();
+    
+       // Read selected files
+       var totalfiles = document.getElementById('files').files.length;
+       for (var index = 0; index < totalfiles; index++) {
+          form_data.append("files[]", document.getElementById('files').files[index]);
+       }
+    
+       // AJAX request
+       $.ajax({
+         url: 'http://x15.dk/hitme.php', 
+         type: 'post',
+         data: form_data,
+         dataType: 'json',
+         contentType: false,
+         processData: false,
+         success: function (response) {
+    
+           for(var index = 0; index < response.length; index++) {
+             var src = response[index];
+    
+             // Add img element in <div id='preview'>
+             $('#preview').append('<img src="'+src+'" width="200px;" height="200px">');
+           }
+         }
+       });
+    });
+    });

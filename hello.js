a61f66643e55a2003b4e56f0878ed6677bdb018c
langehk <32093132+langehk@users.noreply.github.com>
2020-12-07 11:30:00 +0100

    php file

--- hello.js
+++ hello.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var app = express();
 var cors = require("cors");
+const php = require('php');
 
 app.use(cors({ credentials: true, origin: true }));
 app.options("*", cors()); // Allow all origin *
 
 app.set("port", process.env.PORT || 8081);
 app.use(express.static(__dirname + "/"));
+app.set('view engine', 'php');
+app.engine('php', php.__express);
 
 app.get("/", function (request, response) {
   response.send("Hello World!");

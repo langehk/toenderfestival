e85228904d9ee4df6674eb180ee0261aeecbaf5b
langehk <32093132+langehk@users.noreply.github.com>
2020-12-10 09:20:37 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,24 +1,13 @@
-'use strict';
-
 var express = require("express");
-var cors = require("cors");
-var sphp = require('sphp');
-
-
 var app = express();
+var cors = require("cors");
 
-app.set("port", process.env.PORT || 8081);
 
-app.use(sphp.express('./'));
-app.use(express.static('./'));
-
-/*
 app.use(cors({ credentials: true, origin: true }));
 app.options("*", cors()); // Allow all origin *
 
 app.set("port", process.env.PORT || 8081);
-app.use(sphp.express('./'));
-app.use(express.static('./'));
+app.use(express.static(__dirname + "/"));
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
@@ -26,4 +15,4 @@ app.get("/", function (req, res) {
 
 app.listen(app.get("port"), function () {
   console.log("Node app is running at localhost:" + app.get("port"));
-});*/
+});
